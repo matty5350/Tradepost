82273ac9fab371ab44c2eb549b29ed99821e1d5a
WARPED IBUN <84038557+matty5350@users.noreply.github.com>
2023-04-03 19:06:56 +0100

    upload

---
+++ config.cs
@@ -0,0 +1,10 @@
+// FeudalTools Server-ID
+$FTTP::ServerID = 000;
+// Customization
+$FTTP::TradeMessageText = "Press CTRL+T to start trading at this tradepost.";
+$FTTP::TradeMessageTime = 9999;
+$FTTP::Keybind = "ctrl t";
+// Text for missing client mod
+$FTTP::ModMissingCenterText = "Tradepost client-mod missing. Check your system chat.";
+$FTTP::ModMissingSystemText = "This server is using the Tradepost Server Mod. In order to trade here, you need the Tradepost Client Mod. You can install it through YoLauncher.";
+
